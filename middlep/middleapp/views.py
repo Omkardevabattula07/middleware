3d6994759361e8b7b268d2ee2c5a378afec8c87c
omkar <devabattulaosst1909@gmail.com>
2024-06-02 09:13:35 +0530

    Added a basic new things which is basic django

--- middlep/middleapp/views.py
+++ middlep/middleapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def manual_view(request):
+    return render(request,"manual.html")
+def restrict_view(request):
+    return render(request,"restrict.html")
